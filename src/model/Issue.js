dfb8b400b1a237d74757003be5f6a7b0a6d9d4d6
Yoann Battu <yoann.battu2024@campus-eni.fr>
2025-01-30 13:09:51 +0100

    feat (comments): create detailed issue

--- src/model/Issue.js
+++ src/model/Issue.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const issueSchema = new Schema({
-  id: String, 
-  title: String, 
-  author: String, 
-  date_created: Date, 
-  status: String, 
-  description: String, 
+    title: String,
+    author: String,
+    date_created: Date,
+    status: String,
+    description: String,
+    comments: [{ com_author: String, com_date: Date, com_message: String }],
 });
 
-const Issue = model('Issue', issueSchema);
+const Issue = model("Issue", issueSchema);
 module.exports = Issue;
